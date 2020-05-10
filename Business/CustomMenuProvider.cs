e890d70e5a14ce39c6482ee1cc9d09a87008dc5d
Alex Schouls <alex_schouls@live.com>
2020-05-10 17:00:35 +0200

    Implemented custom global menu provider

---
+++ Business/CustomMenuProvider.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Shell.Navigation;
+using System.Collections.Generic;
+
+namespace Alloy.Business
+{
+    [MenuProvider]
+    public class CustomMenuProvider : IMenuProvider
+    {
+        public IEnumerable<MenuItem> GetMenuItems()
+        {
+            return new[]
+            {
+                new UrlMenuItem("Settings", "/global/settings", "/backend")
+            };
+        }
+    }
+}
